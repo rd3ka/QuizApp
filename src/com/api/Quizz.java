afef3239b808be26ec9111722a3ff0998fdd7759
rdeka <raktimdeka14@gmail.com>
2022-09-11 22:09:11 +0530

    adding more attributes

--- src/com/api/Quizz.java
+++ src/com/api/Quizz.java
@@ -8,10 +8,12 @@ import java.util.*;
 
 public class Quizz {
     private String url = "https://opentdb.com/api.php?";
-    private final int NumOfQuestions = 10;
+    private int NumOfQuestions = 10;
     final private Stack <qData> qstack = new Stack<>();
     final private Map <Character, String> q2a = new HashMap<>();
     private qData qstackFrame;
+
+    private int score = 0;
 
     Quizz(String category, String difficulty, String type) {
         this.url = this.url.concat("amount=" + this.NumOfQuestions
@@ -63,17 +65,33 @@ public class Quizz {
             return false;
         this.q2a.clear();
         this.qstackFrame = this.qstack.pop();
+        this.NumOfQuestions -= 1;
         return true;
     }
-
+    public synchronized int getNumOfQuestions() {
+        return this.NumOfQuestions;
+    }
     public synchronized void showQuestions() {
         System.out.println(this.qstackFrame.getQuestion());
     }
 
     public synchronized boolean checkAnswer(Character answer) {
-        return this.qstackFrame.checkCorrectAnswer(q2a.get(answer));
+        boolean b =  this.qstackFrame.checkCorrectAnswer(q2a.get(answer));
+        if (b)
+            grantScore();
+        else
+            reduceScore();
+        return b;
     }
-
+    private synchronized void grantScore() {
+        this.score += 10;
+    }
+    private synchronized void reduceScore() {
+        this.score -= 10;
+    }
+    public synchronized int getScore() {
+        return this.score;
+    }
     public void showAnswers() {
         ArrayList <String> qans = this.qstackFrame.getIncorrectAnswers();
         qans.add(this.qstackFrame.getCorrectAnswer());
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,6 +1,8 @@
 package com.main;
 
 import com.api.Quizz;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
 
@@ -15,14 +17,23 @@ public class Main {
               if (toSend < 'a' || toSend > 'd') continue;
               System.out.println(q.checkAnswer(toSend));
           }
-          catch (NumberFormatException exception) {
-              System.out.println("Wrong Format Provided");
+          catch (InputMismatchException exception) {
+              System.out.println(exception.toString());
           }
+          char play = 0;
+          if (q.getNumOfQuestions() == 0) {
+              System.out.print("Do you want to continue to play? y/n : ");
+              play = new Scanner(System.in).nextLine().charAt(0);
+              System.out.println();
+          }
+          if (play == 'y')
+              q.init();
       }
     }
 
     public static void main(String[] args)  {
         Quizz q = new Quizz("multiple");
         play(q);
+        System.out.println("GAME OVER!");
     }
 }
